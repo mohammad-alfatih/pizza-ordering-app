0a29a60f71f5e5b1d85ca59d5d4de38420fad3dc
mohammad-alfatih <alfaitha@gmail.com>
2018-05-19 21:03:05 -0400

    chore: added menu module

---
+++ src/app/menu/menu.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuItemComponent } from './menu-item/menu-item.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MenuItemComponent]
+})
+export class MenuModule { }
