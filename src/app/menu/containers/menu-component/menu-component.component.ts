894b61882025ff722b35c2fbbaf56b621c0557b1
mohammad-alfatih <alfaitha@gmail.com>
2018-05-21 09:39:19 -0400

    chore: build menu-component & template

--- src/app/menu/containers/menu-component/menu-component.component.ts
+++ src/app/menu/containers/menu-component/menu-component.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+import * as actions from '../../store/menu.actions';
+import * as fromMenu from '../../store/menu.reducer';
+import { MenuItem } from '../../store/menu.reducer';
+
+import { Observable } from 'rxjs/Observable';
+
 @Component({
   selector: 'app-menu-component',
   templateUrl: './menu-component.component.html',
   styleUrls: ['./menu-component.component.css']
 })
-export class MenuComponentComponent implements OnInit {
+export class MenuComponent implements OnInit {
+  menuItemList$: Observable<MenuItem[]>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private store: Store<fromMenu.State>) {
+    this.store.dispatch(new actions.Query());
   }
 
+  ngOnInit() {
+    this.menuItemList$ = this.store.select(fromMenu.selectAll);
+  }
+
+  createItem() {
+    const menuItem: fromMenu.MenuItem = {
+      id: new Date().getUTCMilliseconds().toString(),
+      name: 'cheese pizza',
+      category: 'pizza',
+      description: 'amazing Italian treat',
+      imgUrl: 'http://green-wife-horse.wikia.com/wiki/File:Pizza-margarita.png',
+      price: 12,
+      ordered: false
+    };
+
+    this.store.dispatch(new actions.Create(menuItem));
+  }
+
+  updateItem(id: string, prop: Partial<MenuItem>) {
+    this.store.dispatch(new actions.Update(id, prop));
+  }
+
+  deleteItem(id: string) {
+    this.store.dispatch(new actions.Delete(id));
+  }
 }
